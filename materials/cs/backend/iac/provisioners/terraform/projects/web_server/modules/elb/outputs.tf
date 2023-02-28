a4b0ac1925a3a76aad6de789e9d355ef68268179
Gleb Khaykin <khaykingleb@gmail.com>
2023-02-28 18:04:09 +0400

    chore: del folder

--- materials/cs/backend/iac/provisioners/terraform/projects/web_server/modules/elb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dns_name" {
-  value       = aws_elb.this.dns_name
-  description = "DNS name of the Elastic Load Balancer"
-}
-
-output "name" {
-  value       = aws_elb.this.name
-  description = "Name of the Elastic Load Balancer"
-}
--- materials/cs/backend/iac/provisioners/terraform/projects/web_server/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "site_url" {
-  value       = "http://${module.elastic_load_balancer.dns_name}"
-  description = "Site URL"
-}
